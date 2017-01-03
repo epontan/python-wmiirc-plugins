9859206f4defb7e6c298c4e7fb5838d6bc0e27be
Pontus Andersson <epontan@gmail.com>
2017-01-03 14:03:39 +0100

    Update to Spotify's new D-Bus API calls

--- spotify.py
+++ spotify.py
@@ -1,55 +1,63 @@
 import dbus
+import traceback
 
 from plugins.dbus_instance import get_session_bus
 from plugins.dialog import check_dialog, dialog
 
+_BUS = 'org.mpris.MediaPlayer2.spotify'
+_PATH = '/org/mpris/MediaPlayer2'
+_IFACE = 'org.mpris.MediaPlayer2.Player'
 
+def safe(function):
+    def _():
+        try:
+            function()
+        except Exception, e:
+            traceback.print_exc(e)
+    return _
+
+
+@safe
 def toggle_meta_info():
     if check_dialog(__name__):
         return
-    _spotify_player('GetMetadata', _handle_meta_data_reply)
-
-def _handle_meta_data_reply(info):
+    info = spotify_property().Metadata
     artist = ', '.join((s for s in info['xesam:artist']))
     album = info['xesam:album']
     title = info['xesam:title']
-    year = str(info['xesam:contentCreated'])[:4]
     length = long(info['mpris:length']) / 1000000L
     max_width = max(16, len(title), len(album) + 7, len(artist))
-    dialog(u'Spotify%0*s\n%s\n%s\n%s (%s)\n\n%s'
+    dialog(u'Spotify%0*s\n%s\n%s\n%s\n\n%s'
         % (max_width - 7, '[%02d:%02d]' % (length / 60, length % 60),
-           '-' * max_width, artist, album, year, title) , __name__)
+           '-' * max_width, artist, album, title) , __name__)
 
 
+@safe
 def play_pause():
-    _spotify_player('PlayPause')
+    spotify().PlayPause()
 
-
+@safe
 def stop():
-    _spotify_player('Stop')
+    spotify().Stop()
 
-
+@safe
 def prev():
-    _spotify_player('Previous')
+    spotify().Previous()
 
-
+@safe
 def next():
-    _spotify_player('Next')
+    spotify().Next()
 
-
+@safe
 def quit():
-    _spotify_player('Quit')
+    spotify().Quit()
 
 
-def _handle_pass(*args):
-    pass
+def spotify():
+    return bus().get_interface(_IFACE)
 
-def _spotify_player(method, reply_handler=_handle_pass):
-    try:
-        player = get_session_bus('com.spotify.qt', '/').get_interface(
-                    'org.freedesktop.MediaPlayer2')
-        getattr(player, method)(
-                reply_handler=reply_handler,
-                error_handler=_handle_pass)
-    except:
-        pass
+def spotify_property():
+    return bus().get_prop_interface(_IFACE)
+
+def bus():
+    return get_session_bus(_BUS, _PATH)
